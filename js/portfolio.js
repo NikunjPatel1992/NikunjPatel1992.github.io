c0979bd7bc445d8b2b5b9818f295208623e96693
nikunj <nikupatel199@gmail.com>
2019-02-23 15:17:37 -0600

    nikunj

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,5 +1,4 @@
 
-
 var config = {
     apiKey: "AIzaSyB9HkbhGjc_PvxMR87hi3G1ttTjCpFaWpg",
     authDomain: "portfolio-70131.firebaseapp.com",
@@ -15,31 +14,79 @@ var database = firebase.database();
 var name = "";
 var email = "";
 var message = "";
+var subject = "";
 var adddata = "";
 
 
 $(".submit").on("click", function (e) {
     e.preventDefault();
 
-    name = $("#name").val().trim();
-    email = $("#email").val().trim();
-    message = $("#message").val().trim();
+    name = $("#name").val();
+    email = $("#email").val();
+    message = $("#message").val();
+    subject = $("#subject").val();
 
-    if (name == "" || email == "" || message == "") {
+    if (name == "" || email == "" || message == "" || subject == "") {
         alert("Enter Data");
-    } else if (name != "" || email != "" || message != "") {
+    } else if (name != "" || email != "" || message != "" || subject != "") {
         alert("Data Send Successfully");
         database.ref().push({
             Name: name,
             Email: email,
-            Message: message
+            Message: message,
+            Subject: subject
         });
     }
     window.location.reload();
     $("#name").empty();
     $("#email").empty();
+    $("#subject").empty();
     $("#message").empty();
 });
+
+// scroll down animation code
+window.sr = ScrollReveal();
+    sr.reveal('.navbar', {
+      duration: 2000,
+      origin: 'bottom'
+    });
+    sr.reveal('.showcase-left', {
+      duration: 3000,
+      origin: 'left',
+      distance: '300px'
+    });
+    sr.reveal('.showcase-right', {
+      duration: 3000,
+      origin: 'right',
+      distance: '300px'
+    });
+    sr.reveal('.showcase-center', {
+      duration: 2000,
+      delay: 1000,
+      origin: 'bottom'
+    });
+    sr.reveal('.showcase-top', {
+      duration: 2000,
+      origin: 'top',
+      distance: '300px'
+    });
+    sr.reveal('.showcase-main', {
+      duration: 2000,
+      origin: 'top',
+      distance: '150px'
+    });
+    sr.reveal('.info-right', {
+        duration: 2000,
+        origin:'right',
+        distance:'300px',
+        viewFactor: 0.2
+      });
+      sr.reveal('.info-left', {
+        duration: 2000,
+        origin:'left',
+        distance:'300px',
+        viewFactor: 0.2
+      });
 
 // database.ref().on("child_added", function (snapshot) {
 //     var sp = snapshot.val();
